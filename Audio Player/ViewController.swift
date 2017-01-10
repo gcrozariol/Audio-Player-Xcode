270f16cd01a6c08f9d12676f58a72f01b5dbcb1f
Guilherme Crozariol <guilherme@guilhermes-macbook-pro.local>
2017-01-10 15:33:39 -0200

    Audio Player.

--- Audio Player/ViewController.swift
+++ Audio Player/ViewController.swift
@@ -7,19 +7,91 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    var player = AVAudioPlayer()
+    let audioPath = Bundle.main.path(forResource: "nao_estou_normal", ofType: "mp3")
+    
+    @IBOutlet weak var labelDuration: UILabel!
+    @IBOutlet weak var labelRemaining: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var scrubber: UISlider!
+    
+    func updateScrubber() {
+        scrubber.value = Float(player.currentTime)
+    }
+    
+    func updateTimeLabels() {
+        
+        let currentMinute = Int(player.currentTime / 60)
+        let currentSecond = Int(player.currentTime.truncatingRemainder(dividingBy: 60))
+        
+        if currentSecond >= 10 {
+            labelDuration.text = "\(currentMinute):\(currentSecond)"
+        } else {
+            labelDuration.text = "\(currentMinute):0\(currentSecond)"
+        }
+        
+    }
+    
+    @IBAction func scrubberMoved(_ sender: Any) {
+        player.currentTime = TimeInterval(scrubber.value)
+        updateScrubber()
+        updateTimeLabels()
+    }
+    
+    @IBAction func sliderMoved(_ sender: Any) {
+        player.volume = slider.value
+    }
+    
+    @IBAction func play(_ sender: Any) {
+        player.play()
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimeLabels), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        player.pause()
+        timer.invalidate()
+    }
+
+    @IBAction func stop(_ sender: Any) {
+        updateScrubber()
+        updateTimeLabels()
+        timer.invalidate()
+        player.pause()
+        player.currentTime = 0
+    }
+    
+    @IBAction func forward(_ sender: Any) {
+        player.currentTime += 5
+    }
+    
+    @IBAction func backward(_ sender: Any) {
+        player.currentTime -= 5
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            scrubber.maximumValue = Float(player.duration)
+            updateTimeLabels()
+            
+        } catch {
+            
+            // Process some error that might occur
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
